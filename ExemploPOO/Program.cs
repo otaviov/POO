99454437594564f3e30f834e7e63cbe98adffbf3
Otavio Oliveira <71568294+otaviov@users.noreply.github.com>
2021-11-30 02:57:42 -0300

    Add files via upload

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa
+        }
+    }
+}
